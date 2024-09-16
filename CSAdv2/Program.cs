bfcb8ddde3e0c2ff079bf70fb3f051befca3dbd8
wlgus123 <mail30019794@gmail.com>
2024-09-16 23:12:31 +0900

    IComparable 인터페이스 구현 실습

--- CSAdv2/Program.cs
+++ CSAdv2/Program.cs
@@ -8,8 +8,40 @@ namespace CSAdv2
 {
     internal class Program
     {
+        class Product : IComparable<Product>
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(Product other)
+            {
+                return this.Name.CompareTo(other.Name);
+            } 
+
+            // ToString() 구현
+            public override string ToString()
+            {
+                return string.Format("Name: {0}, Price: {1}", Name, Price);
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Product의 리스트 생성하기
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Name = "감자", Price = 1000 },
+                new Product() { Name = "고구마", Price = 1400 },
+                new Product() { Name = "양파", Price = 1200 },
+                new Product() { Name = "당근", Price = 1600 },
+                new Product() { Name = "고추", Price = 1800 }
+            };
+            products.Sort();    // 정렬
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
